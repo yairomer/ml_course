5be83345e9b988f0190031d3a9627af78754b471
Omer Yair <omeryair@gmail.com>
2019-04-18 00:11:57 +0000

    Added workshop 5

--- generate.sh
+++ generate.sh
@@ -13,5 +13,8 @@ jupyter nbconvert --to html --output-dir=$repo_dir/html/ $repo_dir/workshops/wor
 ## Workshop 4
 jupyter nbconvert --to html --output-dir=$repo_dir/html/ $repo_dir/workshops/workshop_04.ipynb
 
+## Workshop 5
+jupyter nbconvert --to html --output-dir=$repo_dir/html/ $repo_dir/workshops/workshop_05.ipynb
+
 ## Slide to pdf
 # `npm bin`/decktape reveal -s 1440x900 http://localhost:8080/slides/workshop.html?id=01 /tmp/workshop_01.pdf 
