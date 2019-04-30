109389865f6063c86f5aeb976958455bf074a5ff
Omer Yair <omeryair@gmail.com>
2019-04-30 10:55:44 +0000

    More updates to clean up code

--- generate.sh
+++ generate.sh
@@ -20,4 +20,5 @@ jupyter nbconvert --to html --output-dir=$repo_dir/html/ $repo_dir/workshops/wor
 # `npm bin`/decktape reveal -s 1440x900 http://localhost:8080/slides/workshop.html?id=01 /tmp/workshop_01.pdf 
 
 ## Assignment 1
-jq --indent 1 '(.cells[] | select(has("outputs")) | .outputs) = []' $repo_dir/assignments/homework_1.ipynb > $repo_dir/assignments/homework_1_test.ipynb
+jq --indent 1 '(.cells[] | select(has("outputs")) | .outputs) = [] | .cells[].metadata = {} | ((.cells[] | select(has("execution_count")) | .execution_count) = null)' $repo_dir/assignments/homework_1.ipynb > $repo_dir/assignments/homework_1.ipynb.tmp
+mv $repo_dir/assignments/homework_1.ipynb.tmp $repo_dir/assignments/homework_1.ipynb
